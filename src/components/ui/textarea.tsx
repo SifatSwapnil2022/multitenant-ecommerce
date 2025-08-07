b9aaab6281e38c1650208691ff503ad7881b9f26
SifatSwapnil2022 <mdsifatullahsheikh@gmail.com>
2025-08-07 13:24:46 +0600

    fix text area

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -4,17 +4,19 @@ import { cn } from "@/lib/utils";
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.TextareaHTMLAttributes<HTMLTextAreaElement>
->(({ className, ...props }, ref) => {
+>(({ className, value, onChange, ...props }, ref) => {
   return (
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border  px-3 py-2 text-base transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
-        // modify the textarea styles
+        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border px-3 py-2 text-base transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         "md:text-base bg-white",
         className
       )}
       ref={ref}
+      value={value}
+      onChange={onChange}
+      {...(value !== undefined && !onChange ? { readOnly: true } : {})}
       {...props}
     />
   );
